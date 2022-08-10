803137f6b87b13b490be0b81703bc01ddf43dbc8
Laurel Carter <laur.m.carter@gmail.com>
2022-08-10 11:00:15 -0700

    tracking player position

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
 
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
@@ -18,9 +21,11 @@ def main() -> None:
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
+        # game loop
         while True:
-            root_console.print(x=1, y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="@")
 
+            # update console
             context.present(root_console)
 
             for event in tcod.event.wait():
